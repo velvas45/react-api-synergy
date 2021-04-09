986153028c77f5e12fe0785d73d2497f2014ad7b
velvas45 <helmiagustiawan905@gmail.com>
2021-04-09 10:01:57 +0700

    update

---
+++ src/Routes.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Switch, Route } from "react-router-dom"
+
+function Routes() {
+    return (
+        <Switch>
+            <Route>
+
+            </Route>
+            <Route>
+                
+            </Route>
+            <Route>
+                
+            </Route>
+            <Route>
+                
+            </Route>
+            <Route>
+                
+            </Route>
+            
+        </Switch>
+    )
+}
+
+export default Routes
